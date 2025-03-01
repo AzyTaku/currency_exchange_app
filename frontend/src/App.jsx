adead3dd0f051f51ab87fb7f25ffe657f94a74d4
AzyTaku <azlanshaveenraban1@gmail.com>
2025-03-01 15:54:15 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-500 border border-spacing-2 border-black text-white'>
+        Helloooo WORLD!
+      </div>
+    </>
+  )
+}
+
+export default App
